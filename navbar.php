ee6e8ed69f84c654c77cba91927e1568d8fbfa63
Titouan Lacombe--Fabre <titouan.lacombe--fabre@etu.univ-cotedazur.fr>
2022-12-18 14:58:28 +0100

    Struncture html static, mise en place de la navbar

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<nav>
+    <h1><span>MANGA</span><span>FLOW</span></h1>
+    <form action="">
+        <input type="search" pattern="ex : BOICHI">
+        <select name="categories">
+            <option valeur="other">Manga</option>
+            <option valeur="nl">Auteur</option>
+            <option valeur="fr">Type</option>
+            <option valeur="en">Genre</option>
+        </select>
+        <input type="submit">
+    </form>
+</nav>
