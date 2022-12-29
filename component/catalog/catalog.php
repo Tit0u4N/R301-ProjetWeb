a26d50ba1204662524e1ce619ed9bcbdb8578f3c
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2022-12-29 19:46:27 +0100

    Test 1

--- component/catalog/catalog.php
+++ component/catalog/catalog.php
@@ -4,6 +4,13 @@
 
     if (isset($_GET['dev'])) {
         if ($_GET['dev'] == True) {
+
+            $mysqli = new mysqli("165.227.152.225", "p3", "FicsiT22!", "db",3306);
+
+            $result = $mysqli->query("SELECT id FROM produit ORDER BY id ASC");
+            foreach ($result -> $ke as $va ){
+                echo "<p>".$ke." /".$va."</p>";
+            }
             $naruto = new Manga("Naruto","Kishimoto Masashi","Kana","Shonen","Action");
             $testTome1 = new Tome($naruto, 1,"03/03/2000","Naruto est un garçon un peu spécial. Il est toujours tout seul et son caractère fougueux ne l'aide pas vraiment à se faire apprécier dans son village. Malgré cela, il garde au fond de lui une ambition: celle de devenir un maître Hokage, la plus haute distinction dans l'ordre des ninjas, et ainsi obtenir la reconnaissance de ses pairs.", "https://www.nautiljon.com/images/manga_volumes/00/59/595.webp?1638617300","6,50",1);
             $testTome2 = new Tome($naruto, 2,"06/03/2000","Naruto est un garçon un peu spécial. Il est toujours tout seul et son caractère fougueux ne l'aide pas vraiment à se faire apprécier dans son village. Malgré cela, il garde au fond de lui une ambition: celle de devenir un maître Hokage, la plus haute distinction dans l'ordre des ninjas, et ainsi obtenir la reconnaissance de ses pairs.", "https://www.nautiljon.com/images/manga_volumes/00/67/1176.webp?1585684568","6,50",2);
