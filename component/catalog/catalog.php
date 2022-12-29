bc03cfcaf057273e6a7f9117da6ab0a0ea768dc7
titouan lacombe--fabre <titouan.lacombe--fabre@etu.univ-cotedazur.fr>
2022-12-29 19:49:39 +0000

    test SQL/PHP

--- component/catalog/catalog.php
+++ component/catalog/catalog.php
@@ -4,6 +4,9 @@
 
     if (isset($_GET['dev'])) {
         if ($_GET['dev'] == True) {
+
+
+
             $naruto = new Manga("Naruto","Kishimoto Masashi","Kana","Shonen","Action");
             $testTome1 = new Tome($naruto, 1,"03/03/2000","Naruto est un garçon un peu spécial. Il est toujours tout seul et son caractère fougueux ne l'aide pas vraiment à se faire apprécier dans son village. Malgré cela, il garde au fond de lui une ambition: celle de devenir un maître Hokage, la plus haute distinction dans l'ordre des ninjas, et ainsi obtenir la reconnaissance de ses pairs.", "https://www.nautiljon.com/images/manga_volumes/00/59/595.webp?1638617300","6,50",1);
             $testTome2 = new Tome($naruto, 2,"06/03/2000","Naruto est un garçon un peu spécial. Il est toujours tout seul et son caractère fougueux ne l'aide pas vraiment à se faire apprécier dans son village. Malgré cela, il garde au fond de lui une ambition: celle de devenir un maître Hokage, la plus haute distinction dans l'ordre des ninjas, et ainsi obtenir la reconnaissance de ses pairs.", "https://www.nautiljon.com/images/manga_volumes/00/67/1176.webp?1585684568","6,50",2);
@@ -22,6 +25,38 @@
     <?php
         if (isset($_GET['dev'])) {
             if ($_GET['dev'] == True) {
+
+                $mysqli = new mysqli("165.227.152.225", "p2", "FicsiT22!", "db",3306);
+
+                $index = rand(1,9);
+                $result = $mysqli->query("SELECT * FROM MANGA m WHERE m.idManga  =".$index);
+                $result->data_seek(1);
+                $mangaSQL = $result->fetch_assoc();
+
+
+                $resultType = $mysqli->query("SELECT t.nom FROM TYPE t WHERE t.idType  = ".$mangaSQL['idType']);
+                $resultType->data_seek(1);
+                $TypeSQL = $resultType->fetch_assoc();
+
+                $mangaTest = new Manga($mangaSQL['nomManga'],$mangaSQL['auteur']," ",$TypeSQL['nom'],"A recup");
+
+                $tomeArray = array();
+                $result = $mysqli->query("SELECT * FROM PRODUIT p WHERE p.idManga =".$mangaSQL['idManga']);
+                for ($row_no = 0 ; $row_no < $result->num_rows; $row_no++) {
+                    $result->data_seek($row_no);
+                    $row = $result->fetch_assoc();
+
+                    $resultImg = $mysqli->query("SELECT * FROM IMAGE i WHERE i.idProduit =".$row['idProduit']);
+                    $resultImg->data_seek(1);
+                    $Img = $resultImg->fetch_assoc();
+
+                    array_push($tomeArray,new Tome($mangaTest,(int) $row['numTome'],$row['dateParution'],$row['description'],$Img['lienImage'],$row['prixPublic'],$row['idProduit']));
+                }
+
+                foreach ($tomeArray as $tome){
+                    $tome->echoHTMLCard();
+                }
+
                 $testTome1->echoHTMLCard();
                 $testTome2->echoHTMLCard();
                 $testTome3->echoHTMLCard();
@@ -42,6 +77,9 @@
     <?php
     if (isset($_GET['dev'])) {
         if ($_GET['dev'] == True) {
+            foreach ($tomeArray as $tome){
+                $tome->echoHTMLDescCard();
+            }
             $testTome1->echoHTMLDescCard();
             $testTome2->echoHTMLDescCard();
             $testTome3->echoHTMLDescCard();
