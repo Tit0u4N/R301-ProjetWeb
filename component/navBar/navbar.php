1792e718d360d9a222a4f21f340e84eceb29943c
tamas <tamas.palotas@etu.univ-cotedazur.fr>
2023-01-07 18:01:25 +0000

    implement return to index

--- component/navBar/navbar.php
+++ component/navBar/navbar.php
@@ -3,7 +3,7 @@
         $fullNavBar = True
 ?>
 <nav>
-    <a class="logo" href=""><span>MANGA</span><span>FLOW</span></a>
+    <a class="logo" href="index.php"><span>MANGA</span><span>FLOW</span></a>
     <?php
         if ($fullNavBar) {
             require "./component/navBar/searchBar.php";
